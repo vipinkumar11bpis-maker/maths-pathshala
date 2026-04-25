007f76884ff444e12fddbaca5eb72b1096669538
vipinkumar11bpis-maker <vipinkumar11b.pis@gmail.com>
2026-04-25 22:51:02 +0530

    Refactor WhatsApp message sending function

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 function sendToWhatsApp() {
-    let name = document.getElementById("name").value;
-    let studentClass = document.getElementById("class").value;
-    let course = document.getElementById("course").value;
-    let subject = document.getElementById("subject").value;
-
-    let message = `New Admission Request:
-Name: ${name}
-Class: ${studentClass}
-Course: ${course}
-Subject: ${subject}`;
-
-    let url = "https://wa.me/919506054813?text=" + encodeURIComponent(message);
-
-    window.open(url, "_blank");
+    let number = "919506054813";
+    let message = "New Inquiry from Maths Pathshala Website";
+    let url = "https://wa.me/" + number + "?text=" + encodeURIComponent(message);
+    window.open(url, '_blank');
 }
