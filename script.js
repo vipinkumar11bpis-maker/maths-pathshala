f2f00e22dadddf0e17d0467af5e7eac41a24203e
vipinkumar11bpis-maker <vipinkumar11b.pis@gmail.com>
2026-04-25 22:54:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function sendToWhatsApp() {
-    let number = "919506054813";
-    let message = "New Inquiry from Maths Pathshala Website";
-    let url = "https://wa.me/" + number + "?text=" + encodeURIComponent(message);
-    window.open(url, '_blank');
+
+let name = document.getElementById("name").value;
+let studentClass = document.getElementById("class").value;
+let course = document.getElementById("course").value;
+let phone = document.getElementById("phone").value;
+
+if(name === "" || studentClass === "" || phone === ""){
+    alert("Please fill all details");
+    return;
+}
+
+let number = "919506054813";
+
+let message = `New Student Inquiry:
+Name: ${name}
+Class: ${studentClass}
+Course: ${course}
+Phone: ${phone}`;
+
+let url = "https://wa.me/" + number + "?text=" + encodeURIComponent(message);
+
+window.open(url, '_blank');
 }
