c552e5ff2ac114c334c5b551863c1f73e6b965ba
vipinkumar11bpis-maker <vipinkumar11b.pis@gmail.com>
2026-04-26 16:33:23 +0530

    Fix counter increment logic and update popup timings

--- script.js
+++ script.js
@@ -1,47 +1,55 @@
-// COUNTER
+// COUNTER FIX (only when visible)
 let counters = document.querySelectorAll(".count");
 
+let startCounter = () => {
 counters.forEach(counter => {
 let update = () => {
 let target = +counter.getAttribute("data-target");
 let count = +counter.innerText;
 
-let inc = target / 100;
+let inc = target / 80;
 
 if(count < target){
 counter.innerText = Math.ceil(count + inc);
 setTimeout(update,20);
-}
-else{
+}else{
 counter.innerText = target;
 }
 }
 update();
 });
+}
 
-// POPUP OPEN
+// trigger when scroll
+window.addEventListener("scroll", () => {
+let section = document.querySelector(".counter-section");
+if(section.getBoundingClientRect().top < window.innerHeight){
+startCounter();
+}
+});
+
+/* POPUP */
 function openPopup(){
 document.getElementById("popupForm").style.display="flex";
 }
 
-// CLOSE
 function closePopup(){
 document.getElementById("popupForm").style.display="none";
 }
 
-// AUTO OPEN AFTER 4s
-setTimeout(openPopup,4000);
+// auto open after 5 sec
+setTimeout(openPopup,5000);
 
-// AUTO CLOSE AFTER 10s
-setTimeout(closePopup,14000);
+// auto close after 12 sec
+setTimeout(closePopup,17000);
 
-// WHATSAPP
+// WhatsApp
 function sendPopup(){
 let name = document.getElementById("pname").value;
 let studentClass = document.getElementById("pclass").value;
 let phone = document.getElementById("pphone").value;
 
-let msg = `New Student:%0AName:${name}%0AClass:${studentClass}%0APhone:${phone}`;
+let msg = `New Enquiry:%0AName: ${name}%0AClass: ${studentClass}%0APhone: ${phone}`;
 
 window.open(`https://wa.me/919506054813?text=${msg}`);
 closePopup();
